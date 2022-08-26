5e05e732148a57f739a5d6d93b51f43ed35210a6
marisalepore <56847204+marisalepore@users.noreply.github.com>
2022-08-26 16:43:42 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+function functionCalled() {
+  window.location = "www.killerbrownie.com/customer-feedback-form" // redirect link
+}
+function googleReview() {
+  window.location = "https://g.page/r/CW8C2rLixGU2EAg/review" // redirect link
+}
